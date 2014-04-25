bfe27de790a4a16df2d833ac8b68a0ee69110810
Mudit Ameta <zeusdeux@gmail.com>
2014-04-26 05:23:04 +0530

    fixed the text to be more explanatory and changed indentation to 2 spaces from 4

--- examples/basic.js
+++ examples/basic.js
@@ -1,19 +1,19 @@
 (function($) {
-	$(document).ready(function() {
-		$('#container > div.box').css('background-color', '#21221E');
-		$('#container > div.box:in-viewport').css('background-color', '#C5C7BC');
+  $(document).ready(function() {
+    $('#container > div.box').css('background-color', '#21221E');
+    $('#container > div.box:in-viewport').css('background-color', '#C5C7BC');
 
-		var t = $('.js-hook').text();
+    var t = $('.js-hook').text();
 
-		$(window).scroll(function() {
-			$('.js-hook').css({
-				'background-color': '#21221E',
-				'color': '#fff'
-			}).text(t);
-			$('.js-hook:in-viewport(100)').css({
-				'background-color': '#C5C7BC',
-				'color': '#151612'
-			}).text('Now in viewport.');
-		});
-	});
+    $(window).scroll(function() {
+      $('.js-hook').css({
+        'background-color': '#21221E',
+        'color': '#fff'
+      }).text(t);
+      $('.js-hook:in-viewport(100)').css({
+        'background-color': '#C5C7BC',
+        'color': '#151612'
+      }).text('Now in viewport.');
+    });
+  });
 }(jQuery));
