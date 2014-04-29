9d80e84896f760974b227d5c06a00aea2a7be4a7
Mudit Ameta <zeusdeux@gmail.com>
2014-04-30 02:28:44 +0530

    fixed for mobile users

--- examples/advanced-viewport.js
+++ examples/advanced-viewport.js
@@ -1,5 +1,15 @@
 (function($) {
   $(document).ready(function() {
+    //this adjusts the width when there isn't any scrollbar (like on a mobile)
+    var divWidth = $('div.box:first').outerWidth();
+
+    //outerWidth to include the massive padding it has
+    //width doesn't work as box-sizing: border-box;
+    //so padding cuts into width
+    //sigh!
+    $('.tolerance').width(divWidth);
+    $('.outOfViewport').css('left',divWidth+'px');
+
     $('#viewport > div.box').css('background-color', '#21221E').text('out of viewport');
     $('#viewport > div.box:in-viewport(100, #viewport)').css('background-color', '#C5C7BC').text('in viewport');
 
