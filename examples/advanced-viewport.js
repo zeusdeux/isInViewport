782ec15719a81537593a52e1de61ced7aca24252
Mudit Ameta <zeusdeux@gmail.com>
2015-11-16 01:04:16 +0530

    Example now uses both pseudo-selector as well as exposed isInViewport fn

--- examples/advanced-viewport.js
+++ examples/advanced-viewport.js
@@ -3,6 +3,9 @@
     //this adjusts the width when there isn't any scrollbar (like on a mobile)
     var divWidth = $('div.box:first').outerWidth();
 
+    // custom viewport
+    var $viewport = $('#viewport');
+
     //outerWidth to include the massive padding it has
     //width doesn't work as box-sizing: border-box;
     //so padding cuts into width
@@ -10,12 +13,16 @@
     $('.tolerance').width(divWidth);
     $('.outOfViewport').css('left',divWidth+'px');
 
-    $('#viewport > div.box').css('background-color', '#21221E').text('out of viewport');
-    $('#viewport > div.box:in-viewport(100, #viewport)').css('background-color', '#C5C7BC').text('in viewport');
+    $viewport.find('div.box').css('background-color', '#21221E').text('out of viewport');
+
+    // using custom viewport with a the "in-viewport" pseudo-selector
+    $viewport.find('div.box:in-viewport(100, #viewport)').css('background-color', '#C5C7BC').text('in viewport');
 
     $('#viewport').scroll(function() {
-      $('#viewport > div.box').css('background-color', '#21221E').text('out of viewport');
-      $('#viewport > div.box:in-viewport(100, #viewport)').css('background-color', '#C5C7BC').text('in viewport');
+      $viewport.find('div.box').css('background-color', '#21221E').text('out of viewport');
+
+      // using custom viewport with the isInViewport function
+      $viewport.find('div.box').isInViewport({ tolerance: 100, viewport: $viewport }).css('background-color', '#C5C7BC').text('in viewport');
     });
   });
 }(jQuery));
